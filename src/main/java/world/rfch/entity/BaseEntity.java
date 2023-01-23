c67c59b495a6f5a81b21109260f1b121fba407bc
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-23 08:48:17 +0400

    BaseEntity

--- src/main/java/world/rfch/entity/BaseEntity.java
+++ src/main/java/world/rfch/entity/BaseEntity.java
@@ -4,8 +4,10 @@ import javax.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
 }
